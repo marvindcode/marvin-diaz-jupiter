0de5411be6d74d6f516e3d26579f06d8d6a3340d
Marvin Diaz <md1685268@gmail.com>
2025-01-18 21:24:52 -0800

    js added, created footer and skills

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
+// Add footer with copyright:
 const today = new Date();
 const thisYear = today.getFullYear();
-
 const footer = document.createElement('footer');
+const copyright = document.createElement('p');
+
+copyright.innerHTML = '&copy; Marvin Diaz 2025';
+footer.appendChild(copyright);
 document.body.appendChild(footer);
 
-const copyright = document.createElement('p');
-copyright.innerHTML = '&copy; Marvin Diaz 2025';
-
-footer.appendChild(copyright);
-
+// Add list of Skills:
 const skills = ["HTML", "CSS", "JavaScript","SQL", "Excel", "Git", "Data Analytics"];
 const skillsSection = document.getElementById('Skills');
 const skillsList = skillsSection.querySelector('ul');
